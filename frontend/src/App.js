7973c4cd0b13b75e3b6ca8ce6120cb1beba1ecc5
SujalPawar <pawarsujal4455@gmail.com>
2026-07-28 22:08:04 +0530

    Added App.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,7 +20,6 @@ function App() {
 
             <Route path='/' element={<LandingPage />} />
             <Route path='/auth' element={<Authentication />} />
-
             <Route path='/home's element={<HomeComponent />} />
             <Route path='/history' element={<History />} />
             <Route path='/:url' element={<VideoMeetComponent />} />
