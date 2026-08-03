1dc587f894b7393af808302ddd645829ebbf3cc4
SujalPawar <pawarsujal4455@gmail.com>
2026-08-03 17:38:11 +0530

    Added history.jsx file

--- frontend/src/pages/history.jsx
+++ frontend/src/pages/history.jsx
@@ -9,6 +9,7 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import HomeIcon from '@mui/icons-material/Home';
 import { IconButton } from '@mui/material';
+
 export default function History() {
 
 
