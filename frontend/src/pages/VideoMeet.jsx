96530caf9301db9de5d043efc910e6396e4a2a79
SujalPawar <pawarsujal4455@gmail.com>
2026-08-06 23:13:02 +0530

    Update VideoMeet.jsx file

---
+++ frontend/src/pages/VideoMeet.jsx
@@ -0,0 +1,5 @@
+import React, { useEffect, useRef, useState } from 'react'
+import io from "socket.io-client";
+import { Badge, IconButton, TextField } from '@mui/material';
+import { Button } from '@mui/material';
+import VideocamIcon from '@mui/icons-material/Videocam';
