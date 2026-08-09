dfef63d6314a04345e12addabd9622b603edec1f
SujalPawar <pawarsujal4455@gmail.com>
2026-08-09 17:38:07 +0530

    Remove unnecessary blank line in landing.jsx

--- frontend/src/pages/landing.jsx
+++ frontend/src/pages/landing.jsx
@@ -28,7 +28,6 @@ export default function LandingPage() {
                 </div>
             </nav>
 
-
             <div className="landingMainContainer">
                 <div>
                     <h1><span style={{ color: "#FF9839" }}>Connect</span> with your loved Ones</h1>
