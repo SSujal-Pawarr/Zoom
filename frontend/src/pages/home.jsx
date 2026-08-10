56a44aef49ab4231a4143a7d5237a4790f3d24f3
SujalPawar <pawarsujal4455@gmail.com>
2026-08-10 19:37:54 +0530

    Remove unused meeting container and associated elements from HomeComponent

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -46,24 +46,7 @@ function HomeComponent() {
 
             </div>
 
-            <div className="meetContainer">
-                <div className="leftPanel">
-                    <div>
-                        <h2>Providing Quality Video Call Just Like Quality Education</h2>
-
-                        <div style={{ display: 'flex', gap: "10px" }}>
-
-                            <TextField onChange={e => setMeetingCode(e.target.value)} id="outlined-basic" label="Meeting Code" variant="outlined" />
-                            <Button onClick={handleJoinVideoCall} variant='contained'>Join</Button>
-
-                        </div>
-
-                    </div>
-                </div>
-                <div className='rightPanel'>
-                    <img srcSet='/logo3.png' alt="" />
-                </div>
-            </div>
+            
         </>
     )
 }
