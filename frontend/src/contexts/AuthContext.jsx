bd0de84ed1dd924a3cee4ed1253e92189f073383
SujalPawar <pawarsujal4455@gmail.com>
2026-08-13 19:28:36 +0530

    Refactor handleLogin function for improved readability and structure

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -37,6 +37,25 @@ export const AuthProvider = ({ children }) => {
         }
     }
 
-    
+    const handleLogin = async (username, password) => {
+        
+        try {
+            let request = await client.post("/login", {
+                username: username,
+                password: password
+            });
 
+            console.log(username, password)
+            console.log(request.data)
+
+            if (request.status === httpStatus.OK) {
+                localStorage.setItem("token", request.data.token);
+                router("/home")
+            }
+        } catch (err) {
+            throw err;
+        }
+    }
+
+    
 }
