576b6006e4ad8376c06e9d916684dfc7a0a72e20
SujalPawar <pawarsujal4455@gmail.com>
2026-08-13 19:29:33 +0530

    Add addToUserHistory function to log user activity

--- frontend/src/contexts/AuthContext.jsx
+++ frontend/src/contexts/AuthContext.jsx
@@ -73,5 +73,20 @@ export const AuthProvider = ({ children }) => {
         }
     }
 
-   
+    const addToUserHistory = async (meetingCode) => {
+
+        try {
+
+            let request = await client.post("/add_to_activity", {
+                token: localStorage.getItem("token"),
+                meeting_code: meetingCode
+            });
+            return request
+        } catch (e) {
+            throw e;
+        }
+    }
+
+    
+
 }
